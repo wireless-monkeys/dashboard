68874e231c9225aae24f3da498d5534bd8644b34
Chantawat Yangtrong <cyredhat@gmail.com>
2022-09-28 16:42:21 +0700

    Add camera feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { CameraFeed } from './components/CameraFeed'
 import Graph from './components/Graph'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="w-screen flex flex-col items-center gap-10 p-10">
       <h1 className="text-6xl font-bold">Dashboard</h1>
       <Graph />
+      <CameraFeed />
     </div>
   )
 }
---
+++ src/components/CameraFeed.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useRef, useState } from 'react'
+import { client } from '../client'
+
+function useBlobUrl(blob: Blob | null): string | null {
+  const blobRef = useRef<Blob | null>(null)
+  const urlRef = useRef<string | null>(null)
+
+  if (blobRef.current !== blob) {
+    blobRef.current = blob
+    urlRef.current = blob ? URL.createObjectURL(blob) : null
+  }
+
+  const lastUrl = urlRef.current
+  useEffect(() => {
+    if (lastUrl === null) return
+    return () => {
+      URL.revokeObjectURL(lastUrl)
+    }
+  }, [lastUrl])
+
+  return urlRef.current
+}
+
+export function CameraFeed() {
+  const [imageBlob, setImageBlob] = useState<Blob | null>(null)
+  const imageUrl = useBlobUrl(imageBlob)
+
+  useEffect(() => {
+    const controller = new AbortController()
+    const cameraStream = client.subscribeCamera(
+      {},
+      { abort: controller.signal }
+    )
+    cameraStream.responses.onNext((response) => {
+      if (response?.image) {
+        setImageBlob(new Blob([response.image]))
+      }
+    })
+    return () => {
+      controller.abort()
+    }
+  }, [])
+
+  return (
+    <div className="flex flex-col items-center gap-10 p-10">
+      <h1 className="text-3xl font-bold">Camera Feed</h1>
+      <img alt="Camera feed" src={imageUrl ? imageUrl : undefined} />
+    </div>
+  )
+}
